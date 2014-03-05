9a1987b2024db3de853949d91fc898a5397bc6c8
Q <mail@quaddy-services.de>
2014-03-05 22:19:07 +0100

    don

--- src/de/quaddyservices/dynamicnightlight/util/ListPreferenceCompat.java
+++ src/de/quaddyservices/dynamicnightlight/util/ListPreferenceCompat.java
@@ -14,31 +14,45 @@ import android.util.AttributeSet;
  */
 public class ListPreferenceCompat extends ListPreference {
 
-    public ListPreferenceCompat(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public ListPreferenceCompat(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    public ListPreferenceCompat(Context context) {
-        super(context);
-    }
+	public ListPreferenceCompat(Context context) {
+		super(context);
+	}
 
-    // NOTE:
-    // The framework forgot to call notifyChanged() in setValue() on previous versions of android.
-    // This bug has been fixed in android-4.4_r0.7.
-    // Commit: platform/frameworks/base/+/94c02a1a1a6d7e6900e5a459e9cc699b9510e5a2
-    // Time: Tue Jul 23 14:43:37 2013 -0700
-    //
-    // However on previous versions, we have to workaround it by ourselves.
-    @Override
-    public void setValue(String value) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            super.setValue(value);
-        } else {
-            String oldValue = getValue();
-            super.setValue(value);
-            if (!TextUtils.equals(oldValue, value)) {
-                notifyChanged();
-            }
-        }
-    }
+	// NOTE:
+	// The framework forgot to call notifyChanged() in setValue() on previous versions of android.
+	// This bug has been fixed in android-4.4_r0.7.
+	// Commit: platform/frameworks/base/+/94c02a1a1a6d7e6900e5a459e9cc699b9510e5a2
+	// Time: Tue Jul 23 14:43:37 2013 -0700
+	//
+	// However on previous versions, we have to workaround it by ourselves.
+	@Override
+	public void setValue(String value) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			super.setValue(value);
+		} else {
+			String oldValue = getValue();
+			super.setValue(value);
+			if (!TextUtils.equals(oldValue, value)) {
+				notifyChanged();
+			}
+		}
+	}
+
+	@Override
+	public void setSummary(CharSequence summary) {
+		super.setSummary(summary);
+	}
+
+	@Override
+	public CharSequence getSummary() {
+		CharSequence tempSummary = super.getSummary();
+		if (tempSummary.equals("%s")) {
+			tempSummary = "50%";
+		}
+		return tempSummary;
+	}
 }
