25c6063155a1a1dde7c96d2f0088c414f521ec4e
Stefan Cordes <mail@quaddy-services.de>
2017-01-24 00:52:10 +0100

    Takeover animation from v1

--- app/src/main/java/de/quaddyservices/dynamicnightlight/MainActivity.java
+++ app/src/main/java/de/quaddyservices/dynamicnightlight/MainActivity.java
@@ -1,21 +1,26 @@
 package de.quaddyservices.dynamicnightlight;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,10 +44,145 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private Runnable runnable;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(getClass().getName(), "onStart:" + this);
+        initPowerConnectionReceiver();
+        super.onStart();
+        if (runnable == null) {
+            runnable = new Runnable() {
+                public void run() {
+                    Log.d(getClass().getName(), "timer");
+                    startTimer();
+                }
+            };
+        }
+        startTimer();
+    }
+
+    private  int offsetX = 1;
+    private  int countX = 0;
+    private final int maxCount = 60;
+
+    private int countColor = 0;
+    private int offsetColor = 1;
+
+    private final Handler handler = new Handler();
+
+    private void startTimer() {
+        TextView tempTopText = (TextView) findViewById(R.id.textTop);
+        TextView tempLeftText = (TextView) findViewById(R.id.textLeft);
+        TextView tempRightText = (TextView) findViewById(R.id.textRight);
+        BigTextButton tempEditText = (BigTextButton) findViewById(R.id.BigTextButton1);
+
+        countX = countX + offsetX;
+        if (countX <= 0) {
+            offsetX = 1;
+        } else if (countX >= maxCount) {
+            offsetX = -1;
+        }
+        String tempLeft = "";
+        String tempRight = "";
+        for (int i = 0; i < countX; i++) {
+            tempLeft += "i";
+        }
+        for (int i = maxCount; i > countX; i--) {
+            tempRight += "i";
+        }
+
+        tempLeftText.setText(tempLeft);
+        tempRightText.setText(tempRight);
+
+        // FrameLayout.LayoutParams layoutParams =
+        // (android.widget.FrameLayout.LayoutParams) tempEditText
+        // .getLayoutParams();
+        // layoutParams.setMargins(countX, 0, 0, 0);
+        // tempEditText.setLayoutParams(layoutParams);
+
+        String tempString;
+        tempString = DateFormat.getTimeFormat(this).format(
+                new java.util.Date(System.currentTimeMillis()));
+
+        SharedPreferences tempPref = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String tempPrefString = tempPref.getString("pref_brightness", "50");
+        int tempBrightness;
+        try {
+            tempBrightness = Integer.valueOf(tempPrefString);
+        } catch (NumberFormatException e) {
+            tempBrightness = 50;
+        }
+        int tempColourText = 255;
+        tempColourText = (int) Math.round(tempColourText * tempBrightness
+                / 100.0);
+        int tempIntColourText = 0xff000000 + tempColourText * 256 * 256
+                + tempColourText * 256 + tempColourText;
+        tempEditText.setText(tempString);
+        tempEditText.setColor(tempIntColourText);
+        if (runnable != null) {
+               handler.postDelayed(runnable, 1000);
+        }
+
+        countColor = countColor + offsetColor;
+        if (countColor < -255) {
+            countColor = -255;
+        }
+        if (countColor > 255) {
+            countColor = 255;
+        }
+        if (countColor == -255) {
+            offsetColor = 10;
+        } else if (countColor == 255) {
+            offsetColor = -10;
+        }
+
+        TextView tempBottom = (TextView) findViewById(R.id.textBottom);
+
+        tempTopText.setText("-------");
+        tempBottom.setText("-------");
+
+        int tempColourTop;
+        if (countColor > -100) {
+            tempColourTop = Math.min(255, countColor + 100);
+        } else {
+            tempColourTop = 0;
+        }
+        tempColourTop = (int) Math
+                .round(tempColourTop * tempBrightness / 100.0);
+        int tempIntColTop = 0xff000000 + tempColourTop * 256 * 256
+                + tempColourTop * 256 + tempColourTop;
+        tempTopText.setBackgroundColor(tempIntColTop);
+        tempTopText.setTextColor(tempIntColTop);
+
+        int tempColourBottom;
+        if (countColor < 100) {
+            tempColourBottom = Math.min(255, 100 - countColor);
+        } else {
+            tempColourBottom = 0;
+        }
+        tempColourBottom = (int) Math.round(tempColourBottom * tempBrightness
+                / 100.0);
+        int tempIntColBot = 0xff000000 + tempColourBottom * 256 * 256
+                + tempColourBottom * 256 + tempColourBottom;
+        tempBottom.setBackgroundColor(tempIntColBot);
+        tempBottom.setTextColor(tempIntColBot);
+
+        checkBattery();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(getClass().getName(), "onCreate...");
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        nextCheckBattery = 0;
+        checkBattery();
+        initPowerConnectionReceiver();
+
         return true;
     }
 
@@ -148,6 +288,28 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences tempPref = PreferenceManager
                 .getDefaultSharedPreferences(this);
         return tempPref.getBoolean("pref_ignoreBattery", Boolean.FALSE);
+    }
+
+    private BroadcastReceiver powerConnectionReceiver;
+
+    private void initPowerConnectionReceiver() {
+        if (powerConnectionReceiver == null) {
+            BroadcastReceiver tempPowerConnectionReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    boolean tempCharging = isCharging(intent);
+                    Log.d(getClass().getName(), "PowerConnectionReceiver:"
+                            + intent + " charging=" + tempCharging);
+                    setKeepScreenOn(tempCharging);
+                }
+            };
+            IntentFilter ifilter = new IntentFilter(
+                    Intent.ACTION_POWER_CONNECTED);
+            registerReceiver(tempPowerConnectionReceiver, ifilter);
+            ifilter = new IntentFilter(Intent.ACTION_POWER_DISCONNECTED);
+            registerReceiver(tempPowerConnectionReceiver, ifilter);
+            powerConnectionReceiver = tempPowerConnectionReceiver;
+        }
     }
 
 }
