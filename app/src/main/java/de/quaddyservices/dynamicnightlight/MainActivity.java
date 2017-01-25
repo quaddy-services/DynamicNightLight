0a1c9dc2f3641979e8ce684d764f61b68a0e0e9e
Stefan Cordes <mail@quaddy-services.de>
2017-01-25 23:58:40 +0100

    use getWindow().getDecorView()

--- app/src/main/java/de/quaddyservices/dynamicnightlight/MainActivity.java
+++ app/src/main/java/de/quaddyservices/dynamicnightlight/MainActivity.java
@@ -1,11 +1,13 @@
 package de.quaddyservices.dynamicnightlight;
 
+import android.app.ActionBar;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -31,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private long nextCheckBattery = 0;
     private Timer timer;
-    private int systemUiVisibility;
-    private CoordinatorLayout.Behavior behavior;
+     private CoordinatorLayout.Behavior behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,19 +69,27 @@ public class MainActivity extends AppCompatActivity {
      * https://developer.android.com/training/system-ui/immersive.html
      */
     private void switchToFullscreen() {
-        View contentView = findViewById(R.id.fullscreen_content);
-            Log.i(getClass().getName(), "switch to fullscreen" );
-            systemUiVisibility = contentView.getSystemUiVisibility();
-            contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // http://stackoverflow.com/questions/16291640/how-to-hide-the-soft-key-bar-on-android-phone
+        View decorView = getWindow().getDecorView();
+
+        Log.i(getClass().getName(), "switch to fullscreen " +decorView);
+         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                     | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                     | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
+        // Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+  //      actionBar.hide();
+        //  View contentView = findViewById(R.id.fullscreen_content);
+       // contentView.setFitsSystemWindows(true);
         // http://stackoverflow.com/questions/10444153/android-statusbar-overlay-with-actionbar
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
 
     }
 
